08e36b9a6fb51b5d3906c4756d664ceb78d03a85
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2020-02-14 21:54:44 +0100

    api calls working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,30 @@ import { Provider } from 'react-redux';
 
 import Container from '../Container/Container';
 import store from '../../store';
+import punkApi from '../../apis/punkApi';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Container />
-    </Provider>
-  );
+class App extends React.Component {
+  state = { beers: null };
+
+  componentDidMount() {
+    this.onAppStart()
+  }
+
+  onAppStart = async () => {
+    const response = await punkApi();
+    this.setState({
+      beers: response.data
+    });
+    console.log(this.state.beers);
+  };
+
+  render() {
+    return (
+      <Provider store={store} >
+        <Container />
+      </Provider>
+    );
+  }
 }
 
 export default App;
